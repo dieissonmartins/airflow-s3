fd6321e918fcfcdfd2932e947242268724ac54ea
dieissonmartins <dieisson.martins.santos@gmail.com>
2024-01-30 09:56:07 -0300

    debug

--- dags/EcsRunTaskOperator.py
+++ dags/EcsRunTaskOperator.py
@@ -7,6 +7,7 @@ from airflow.operators.bash import BashOperator
 task_definition = "4e88e5f4144c42e9910ea994a28eef3f"
 ecs_cluster = "airflow-ecs"
 php_script_path_on_container = "/scripts/index.php"
+ecs_subnets = ["subnet-03e9ed8201ae37039"]
 
 # Define your DAG parameters
 default_args = {
@@ -22,10 +23,19 @@ dag = DAG(
     schedule="*/5 * * * *"
 )
 
-run_bash = BashOperator(
+run_task = EcsRunTaskOperator(
     task_id="ECS_RUN_TASK_OPERATOR",
-    bash_command="echo 'runting ECS_RUN_TASK_OPERATOR...'",
-    dag=dag
+    cluster=ecs_cluster,
+    task_definition=task_definition,
+    overrides={
+        'containerOverrides': [
+            {
+                'name': 'run-airflow-ecr',
+                'command': ['/usr/bin/php', php_script_path_on_container],
+            },
+        ],
+    },
+    network_configuration={"awsvpcConfiguration": {"subnets": ecs_subnets}}
 )
 
-run_bash
+run_task
