d1a11ffa48bdadda387261895d36fab4980caf2b
dieissonmartins <dieisson.martins.santos@gmail.com>
2024-01-30 12:14:22 -0300

    task to dags

--- dags/MWGAGS.py
+++ dags/MWGAGS.py
@@ -4,17 +4,67 @@ from airflow.operators.dummy_operator import DummyOperator
 
 # Define the list of tasks or configurations for your DAGs
 dag_configurations = [
-    {"dag_id": "VISOES_BI", "schedule_interval": "0 0 * * *"},
-    {"dag_id": "BI_FORM_ATENDIMENTO", "schedule_interval": "0 2 * * *"},
-    {"dag_id": "VISOES_BI", "schedule_interval": "0 2 * * *"},
-    {"dag_id": "ATUALIZA_FETCHED_FORM_VENCIMENTO", "schedule_interval": "0 2 * * *"},
-    {"dag_id": "BARCODE_TIROLEZ", "schedule_interval": "0 2 * * *"},
-    {"dag_id": "ALERTA_RUPTURA", "schedule_interval": "0 2 * * *"}
+    {
+        "dag_id": "VISOES_BI",
+        "schedule_interval": "0 0 * * *",
+        "tasks": [
+            {
+                "task_id": "VISOES_BI_TASK",
+                "params": "bancos=db_ovosjosidith_qa"
+            },
+            {
+                "task_id": "VISOES_BI_TASK2",
+                "params": "bancos=db_ultracheese,web68db3 dt_ini=D-1 dt_fim=D-1"
+            }
+        ]
+    },
+    {
+        "dag_id": "BI_FORM_ATENDIMENTO",
+        "schedule_interval": "0 0 * * *",
+        "tasks": [
+            {
+                "task_id": "BI_FORM_ATENDIMENTO_TASK",
+                "params": "bancos=db_ovosjosidith_qa"
+            },
+            {
+                "task_id": "BI_FORM_ATENDIMENTO_TASK2",
+                "params": "bancos=db_ultracheese,web68db3 dt_ini=D-1 dt_fim=D-1"
+            }
+        ]
+    },
+    {
+        "dag_id": "VISOES_BI",
+        "schedule_interval": "0 0 * * *",
+        "tasks": [
+            {
+                "task_id": "VISOES_BI_TASK",
+                "params": "bancos=db_ovosjosidith_qa"
+            },
+            {
+                "task_id": "VISOES_BI_TASK2",
+                "params": "bancos=db_ultracheese,web68db3 dt_ini=D-1 dt_fim=D-1"
+            }
+        ]
+    },
+    {
+        "dag_id": "ATUALIZA_FETCHED_FORM_VENCIMENTO",
+        "schedule_interval": "0 0 * * *",
+        "tasks": [
+            {
+                "task_id": "ATUALIZA_FETCHED_FORM_VENCIMENTO_TASK",
+                "params": "bancos=db_ovosjosidith_qa"
+            },
+            {
+                "task_id": "ATUALIZA_FETCHED_FORM_VENCIMENTO_TASK2",
+                "params": "bancos=db_ultracheese,web68db3 dt_ini=D-1 dt_fim=D-1"
+            }
+        ]
+    }
 ]
 
 
 # Define a function to create DAGs dynamically
-def create_dag(dag_id, schedule_interval):
+def create_dag(dag_id, schedule_interval, tasks):
     default_args = {
         'owner': 'Dieisson',
         'depends_on_past': False,
@@ -32,8 +82,11 @@ def create_dag(dag_id, schedule_interval):
         schedule_interval=schedule_interval,
     )
 
-    # Define tasks for each DAG
-    DummyOperator(task_id='task', dag=dag)
+    for task_configuration in tasks:
+        task_id = task_configuration['task_id']
+
+        # Define tasks for each DAG
+        DummyOperator(task_id=task_id, dag=dag)
 
     return dag
 
@@ -42,6 +95,7 @@ def create_dag(dag_id, schedule_interval):
 for config in dag_configurations:
     dag_id = config["dag_id"]
     schedule_interval = config["schedule_interval"]
+    tasks = config["tasks"]
 
     # Call the create_dag function to create the DAG dynamically
-    globals()[dag_id] = create_dag(dag_id, schedule_interval)
+    globals()[dag_id] = create_dag(dag_id, schedule_interval, tasks)
