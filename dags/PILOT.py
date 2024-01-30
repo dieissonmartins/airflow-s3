f7ea5d83ab4725a37f25c8a3b22a3da3baa40d10
dieissonmartins <dieisson.martins.santos@gmail.com>
2024-01-30 11:39:19 -0300

    teste 2 dag no mesma file

--- dags/PILOT.py
+++ dags/PILOT.py
@@ -36,3 +36,23 @@ run_bash = BashOperator(
 )
 
 run >> run_bash
+
+
+dag2 = DAG(
+    "PILOT2",
+    description="PILOT execução DAGs deploy S3",
+    default_args=default_args,
+    schedule="*/5 * * * *",
+    tags=['Github Actions', 's3'],
+    catchup=False,
+    start_date=datetime(2024, 1, 1)
+)
+
+
+run_bash2 = BashOperator(
+    task_id="run_pilot_bash2",
+    bash_command="echo 'runting process2...'",
+    dag=dag2
+)
+
+run_bash2
