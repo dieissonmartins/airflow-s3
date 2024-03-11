10fbd6fabee097980807c4780fc5255c3d6c1c35
dieissonmartins <dieisson.martins.santos@gmail.com>
2024-03-11 18:56:20 -0300

    criada dag para enviar itens a fila

---
+++ dags/DownloadCsvEstacionamentoRotativo.py
@@ -0,0 +1,39 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.providers.amazon.aws.operators.ecs import EcsRunTaskOperator
+
+# Define your ECS task parameters
+ecs_cluster = "airflow-ecs"
+task_definition = "task-definition-bhtrans:2"
+ecs_subnets = ["subnet-01e897958bf6fe9a5"]
+
+dag = DAG(
+    'DOWNLOAD_CSV_ESTACIONAMENTO_ROTARIVO',
+    default_args={
+        'owner': 'Dieisson',
+        'start_date': datetime(2024, 3, 11),
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),
+    },
+    schedule=None
+)
+
+run_task = EcsRunTaskOperator(
+    task_id="DOWNLOAD_CSV_ESTACIONAMENTO_ROTARIVO",
+    cluster=ecs_cluster,
+    task_definition=task_definition,
+    launch_type='FARGATE',
+    overrides={
+        'containerOverrides': [
+            {
+                'name': 'etl-bhtrans',
+                "command": ["php", "scripts/index.php", "DownloadCsvEstacionamentoRotativo"],
+            },
+        ],
+    },
+    network_configuration={"awsvpcConfiguration": {"subnets": ecs_subnets}},
+    aws_conn_id='aws_conn_ecs',
+    dag=dag
+)
+
+run_task
