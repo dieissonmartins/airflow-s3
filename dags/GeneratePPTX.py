8fd0865867376d59688b8d71b2d7086aed6610af
dieissonmartins <dieisson.martins.santos@gmail.com>
2024-03-25 09:33:48 -0300

    debug

---
+++ dags/GeneratePPTX.py
@@ -0,0 +1,39 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.providers.amazon.aws.operators.ecs import EcsRunTaskOperator
+
+# Define your ECS task parameters
+ecs_cluster = "generate-pptx"
+task_definition = "spire-presentation-mw-generate-pptx:1"
+ecs_subnets = ["subnet-01e897958bf6fe9a5"]
+
+dag = DAG(
+    'GERAR_PPTX_DAG',
+    default_args={
+        'owner': 'Dieisson',
+        'start_date': datetime(2024, 3, 11),
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),
+    },
+    schedule=None
+)
+
+run_task = EcsRunTaskOperator(
+    task_id="GERAR_PPTX_DAG",
+    cluster=ecs_cluster,
+    task_definition=task_definition,
+    launch_type='FARGATE',
+    overrides={
+        'containerOverrides': [
+            {
+                'name': 'spire-presentation-mw-generate-pptx',
+                "command": ["python", "src/scripts/generate_pptx/main.py"],
+            },
+        ],
+    },
+    network_configuration={"awsvpcConfiguration": {"subnets": ecs_subnets}},
+    aws_conn_id='aws_conn_ecs',
+    dag=dag
+)
+
+run_task
